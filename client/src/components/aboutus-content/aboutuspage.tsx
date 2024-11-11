6e757130cf43cd5d22acfe03451c8d251a25d71f
Ahmad Ashfaq <spoiledwit@gmail.com>
2024-11-11 18:28:53 +0500

    added route for create rec

--- client/src/components/aboutus-content/aboutuspage.tsx
+++ client/src/components/aboutus-content/aboutuspage.tsx
@@ -1,7 +1,9 @@
-'use client'
+"use client";
 import { FaLinkedin } from "react-icons/fa";
 import { useEffect } from "react";
 import AnimateToView from "@/components/AnimateToView";
+import { useState } from "react";
+import qs from "qs";
 
 const AboutUs = () => {
   useEffect(() => {
@@ -67,6 +69,49 @@ const AboutUs = () => {
     },
   ];
 
+  async function getTeamMembers(): Promise<any[]> {
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+    const path = "/api/teammembers"; // Match your collection name exactly
+    const url = new URL(path, baseUrl);
+  
+    url.search = qs.stringify({
+      populate: {
+        Image: { // Match the field name from your schema (capital 'I')
+          fields: ["url"],
+        },
+      },
+    });
+  
+    const res = await fetch(url.toString());
+    if (!res.ok) {
+      throw new Error("Failed to fetch team members");
+    }
+    const data = await res.json();
+    return data.data;
+  }
+  
+
+  const [teamMembers, setTeamMembers] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    fetchTeamMembers();
+  }, []);
+
+  const fetchTeamMembers = async () => {
+    setLoading(true);
+    try {
+      const data = await getTeamMembers();
+      setTeamMembers(data);
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching team members:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="bg-DB px-4 xl:px-40 md:px-20 pt-40 pb-20 flex flex-col gap-20">
       <div className=" flex flex-col gap-12">
@@ -89,33 +134,35 @@ const AboutUs = () => {
               farmers nationwide.
               <br />
               <br />
-              <span className="text-LG">Products:</span> We offer tailored products to meet the specific needs of
-              dairy farmers, including Bovine Semen, Compound Feed, Minerals,
-              Farm Mechanized Machinery, TMR Wagons, and Hygiene Solutions. Our
-              products enable clients to achieve optimal productivity and
-              profitability on their farms.
+              <span className="text-LG">Products:</span> We offer tailored
+              products to meet the specific needs of dairy farmers, including
+              Bovine Semen, Compound Feed, Minerals, Farm Mechanized Machinery,
+              TMR Wagons, and Hygiene Solutions. Our products enable clients to
+              achieve optimal productivity and profitability on their farms.
               <br />
               <br />
-              <span className="text-LG">Services:</span> Our comprehensive services cover animal health
-              management, nutrition consulting, breeding assistance, and farm
-              management solutions. We prioritize the well-being of farmers,
-              employing the latest practices to ensure farm productivity and
-              longevity.
+              <span className="text-LG">Services:</span> Our comprehensive
+              services cover animal health management, nutrition consulting,
+              breeding assistance, and farm management solutions. We prioritize
+              the well-being of farmers, employing the latest practices to
+              ensure farm productivity and longevity.
               <br />
               <br />
-              <span className="text-LG">Training and Consultancy:</span> We provide training and consultancy
-              services to enhance farmers' skills and improve farm operations.
-              Through workshops, seminars, and personalized consultations, we
-              cover topics such as animal husbandry, feeding practices, disease
-              management, and farm management techniques.
+              <span className="text-LG">Training and Consultancy:</span> We
+              provide training and consultancy services to enhance farmers'
+              skills and improve farm operations. Through workshops, seminars,
+              and personalized consultations, we cover topics such as animal
+              husbandry, feeding practices, disease management, and farm
+              management techniques.
               <br />
               <br />
-              <span className="text-LG">Project Development:</span> In addition to our core offerings, we also
-              specialize in project development for the dairy industry. We
-              provide expertise in setting up dairy farms, establishing
-              efficient processes, and implementing best practices. Our goal is
-              to support the development of successful dairy projects that
-              contribute to the growth and prosperity of the industry.
+              <span className="text-LG">Project Development:</span> In addition
+              to our core offerings, we also specialize in project development
+              for the dairy industry. We provide expertise in setting up dairy
+              farms, establishing efficient processes, and implementing best
+              practices. Our goal is to support the development of successful
+              dairy projects that contribute to the growth and prosperity of the
+              industry.
               <br />
               <br />
               At Solve Agri Pak, we are committed to fostering a thriving dairy
@@ -130,114 +177,90 @@ const AboutUs = () => {
               Solve Agri Pak, Your Agri Partner forever after.
             </p>
           </div>
-          {/* <div className="w-full  h-full items-center justify-center">
-            <img src="/aboutus/img.jpg" alt="" className="w-full min-w-[320px]" />
-          </div> */}
+       
         </div>
-        {/* <AnimateToView>
-          <p className="mt-10 text-indigo-50">
-            What started as a knowledge transfer organization in 2010, with a
-            diverse team of professionals having decades of experience, has now
-            evolved as one of the most dynamic and trustworthy agribusiness
-            partners in the industry having the most diverse portfolio. Today,
-            Solve Agri Pak enjoys the status of a highly reputed solution
-            provider in the industry with a wide range of products and services,
-            representing some of the finest brands of the world, for all tiers
-            of livestock & dairy farmers. Solve Agri Pak offers workable and
-            sustainable solutions designed to cater to the needs of all
-            stakeholders of the dairy, livestock and related agriculture sectors
-            of Pakistan and beyond. We provide a range of products and services
-            to public and private businesses looking for solutions to increase
-            productivity, minimize costs, increase profit margins, alleviate
-            poverty and develop infrastructure.
-          </p>
-        </AnimateToView>
-        <AnimateToView>
-          <p className="mt-10 text-indigo-50">
-            Our uniqueness lies in providing the right combination of
-            high-quality products and adequate knowledge backed with a high
-            level of advisory services to ensure that our clients achieve
-            maximum advantage of our offerings. The company provides
-            consultancy, training, equipment, product supplies and community
-            development services for the agribusiness sectors.
-          </p>
-        </AnimateToView> */}
+        
       </div>
 
       <div className="h-0.5 w-1/2 bg-LG self-center" />
 
       <div className="flex flex-col gap-12">
-  <h2 className="text-center text-3xl font-medium text-LG">
-    Message from the chairman
-  </h2>
-  <div className="w-full flex flex-col lg:flex-row gap-12">
-    <AnimateToView className="w-full flex flex-col items-center justify-center">
-      <div className="relative h-[260px] w-[260px] border p-2 overflow-hidden">
-        {/* Chairman's Image */}
-        <img
-          src="/aboutus/chairman.png"
-          alt="chairman"
-          className="w-full h-full object-cover"
-        />
+        <h2 className="text-center text-3xl font-medium text-LG">
+          Message from the chairman
+        </h2>
+        <div className="w-full flex flex-col lg:flex-row gap-12">
+          <AnimateToView className="w-full flex flex-col items-center justify-center">
+            <div className="relative h-[260px] w-[260px] border p-2 overflow-hidden">
+              {/* Chairman's Image */}
+              <img
+                src="/aboutus/chairman.png"
+                alt="chairman"
+                className="w-full h-full object-cover"
+              />
 
-        {/* LinkedIn Icon inside the image at the bottom-right corner */}
-        <a
-          href="https://www.linkedin.com/in/lodhi-haroon-65b8059/"
-          target="_blank"
-          rel="noreferrer"
-          className="absolute bottom-4 right-4 bg-LG p-2 hover:bg-white rounded-full shadow-lg"
-        >
-          <FaLinkedin className="text-2xl text-white hover:text-LG transition-colors duration-300" />
-        </a>
+              {/* LinkedIn Icon inside the image at the bottom-right corner */}
+              <a
+                href="https://www.linkedin.com/in/lodhi-haroon-65b8059/"
+                target="_blank"
+                rel="noreferrer"
+                className="absolute bottom-4 right-4 bg-LG p-2 hover:bg-white rounded-full shadow-lg"
+              >
+                <FaLinkedin className="text-2xl text-white hover:text-LG transition-colors duration-300" />
+              </a>
+            </div>
+
+            {/* Chairman's Details */}
+            <div className="flex flex-col items-center justify-center text-white mt-4">
+              <p className="text-xl font-medium text-center">
+                Haroon M. K. Lodhi
+              </p>
+              <p className="text-center text-LG text-sm">
+                Chief Executive Officer
+              </p>
+            </div>
+          </AnimateToView>
+
+          <AnimateToView>
+            <p className="w-full text-white text-center lg:text-start">
+              Welcome to Solve Agri Pak! Our mission is to provide customized
+              solutions for Livestock & Agriculture Value-added Enterprises. We
+              are committed to the success of dairy, livestock, and
+              agribusinesses.
+              <br />
+              <br />
+              At Solve Agri Pak, we prioritize serving farmers, value chain
+              players, processors, and consumers in the agribusiness sector. Our
+              consultancy services support farm establishment, management, and
+              value-added product development, while our agronomy support
+              focuses on livestock farming.
+              <br />
+              <br />
+              Through our Solve Agri & Dairy Institute (SADI), we offer
+              comprehensive training and capacity-building services. This
+              empowers livestock and dairy farmers, professionals, and
+              agribusiness entrepreneurs with the latest techniques and skills.
+              <br />
+              <br />
+              Our unique strength lies in our blend of local expertise and
+              international exposure, earning the trust of renowned clients.
+              <br />
+              <br />
+              At Solve Agri Pak, we are passionate about serving our clients.
+              Together, we will drive the growth and success of the livestock
+              and dairy farming industry.
+              <br />
+              <br />
+              Thank you for choosing Solve Agri Pak.
+              <br />
+              <br />
+              Best regards,
+              <br />
+              <br />
+              <span className="text-LG font-medium">Haroon M. K. Lodhi</span>
+            </p>
+          </AnimateToView>
+        </div>
       </div>
-
-      {/* Chairman's Details */}
-      <div className="flex flex-col items-center justify-center text-white mt-4">
-        <p className="text-xl font-medium text-center">Haroon M. K. Lodhi</p>
-        <p className="text-center text-LG text-sm">Chief Executive Officer</p>
-      </div>
-    </AnimateToView>
-
-    <AnimateToView>
-      <p className="w-full text-white text-center lg:text-start">
-        Welcome to Solve Agri Pak! Our mission is to provide customized
-        solutions for Livestock & Agriculture Value-added Enterprises. We are
-        committed to the success of dairy, livestock, and agribusinesses.
-        <br />
-        <br />
-        At Solve Agri Pak, we prioritize serving farmers, value chain players,
-        processors, and consumers in the agribusiness sector. Our consultancy
-        services support farm establishment, management, and value-added product
-        development, while our agronomy support focuses on livestock farming.
-        <br />
-        <br />
-        Through our Solve Agri & Dairy Institute (SADI), we offer comprehensive
-        training and capacity-building services. This empowers livestock and
-        dairy farmers, professionals, and agribusiness entrepreneurs with the
-        latest techniques and skills.
-        <br />
-        <br />
-        Our unique strength lies in our blend of local expertise and
-        international exposure, earning the trust of renowned clients.
-        <br />
-        <br />
-        At Solve Agri Pak, we are passionate about serving our clients. Together,
-        we will drive the growth and success of the livestock and dairy farming
-        industry.
-        <br />
-        <br />
-        Thank you for choosing Solve Agri Pak.
-        <br />
-        <br />
-        Best regards,
-        <br />
-        <br />
-        <span className="text-LG font-medium">Haroon M. K. Lodhi</span>
-      </p>
-    </AnimateToView>
-  </div>
-</div>
-
 
       <div className="h-0.5 w-1/2 bg-LG self-center" />
 
@@ -247,41 +270,41 @@ const AboutUs = () => {
           <h3 className="mt-2">A winning team; Our recipie for success</h3>
         </AnimateToView>
         <div className="grid grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-8 justify-between gap-16">
-  {team.map((member, index) => (
-    <AnimateToView
-      key={index}
-      className="flex flex-col justify-center items-center"
-    >
-      {/* Parent div for image and LinkedIn icon with relative positioning */}
-      <div className="relative w-full overflow-hidden">
-       <div
-       className="w-[300px] h-[300px] overflow-hidden"
-       >
-<img
-src={member.image}
-alt="" />
-       </div>
+          {teamMembers.map((member, index) => (
+            <AnimateToView
+              key={index}
+              className="flex flex-col justify-center items-center"
+            >
+              {/* Parent div for image and LinkedIn icon with relative positioning */}
+              <div className="relative w-full overflow-hidden">
+                <div className="w-[300px] h-[300px] overflow-hidden">
+                  <img src={member.Image.url} alt="" />
+                </div>
 
-        {/* LinkedIn icon positioned at the bottom-right corner */}
-        <a
-          href={member.linkedIn}
-          target="_blank"
-          rel="noreferrer"
-          className="absolute bottom-4 right-4 bg-LG hover:bg-white p-2 rounded-full shadow-lg"
-        >
-          <FaLinkedin className="text-2xl text-white hover:text-LG transition-colors duration-300" />
-        </a>
-      </div>
+                {/* LinkedIn icon positioned at the bottom-right corner */}
+                <a
+                  href={member.LinkedIn}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="absolute bottom-4 right-4 bg-LG hover:bg-white p-2 rounded-full shadow-lg"
+                >
+                  <FaLinkedin className="text-2xl text-white hover:text-LG transition-colors duration-300" />
+                </a>
+              </div>
 
-      {/* Text content for the team member */}
-      <div className="flex flex-col items-center justify-center text-white mt-4">
-        <h2 className="text-xl font-medium text-center whitespace-nowrap">{member.name}</h2>
-        <p className="text-center whitespace-nowrap">{member.designation}</p>
-      </div>
-    </AnimateToView>
-  ))}
-</div>
-
+              {/* Text content for the team member */}
+              <div className="flex flex-col items-center justify-center text-white mt-4">
+                <h2 className="text-xl font-medium text-center whitespace-nowrap">
+                  {member.Name}
+                </h2>
+                <p className="text-center whitespace-nowrap">
+                  {member.Designation}
+                </p>
+              </div>
+              
+            </AnimateToView>
+          ))}
+        </div>
       </div>
     </div>
   );
