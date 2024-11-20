5bab6edea3a3cee94f833ed1b0c58bde2059e771
Ahmad Ashfaq <spoiledwit@gmail.com>
2024-11-20 17:05:52 +0500

    transferred entities

--- server/config/database.ts
+++ server/config/database.ts
@@ -1,8 +1,8 @@
 import path from 'path';
 
 export default ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
-
+  const client = env('DATABASE_CLIENT', 'postgres');
+  
   const connections = {
     mysql: {
       connection: {
@@ -24,21 +24,14 @@ export default ({ env }) => {
     },
     postgres: {
       connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
+        database: env('DATABASE_NAME'),
         user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+        password: env('DATABASE_PASSWORD'),
+        ssl: {
+          rejectUnauthorized: false
         },
-        schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
--- server/config/plugins.ts
+++ server/config/plugins.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+export default () => ({
+    'strapi-neon-tech-db-branches': {
+    enabled: true,
+    config: {
+      neonApiKey: "rlen131bdcbnn6p68ijlh0p31wtg1tzk040w58fsok6ez9sefvoylwrjv6m3jca2", // get it from here: https://console.neon.tech/app/settings/api-keys
+      neonProjectName: "solveagri", // the neon project under wich your DB runs
+      neonRole: "neondb_owner", // create it manually under roles for your project first
+      //(gitBranch: "main") // branch can be pinned via this config option. Will not use branch from git then. Usefull for preview/production deployment
+    }
+  },
+});
